54c6e1f5c661cb8574c303983bc425dbf891cd42
bispo-daniel <dbispo.work@gmail.com>
2023-03-17 14:56:33 -0300

    updatev2
    Removed some duplicates try...catch, updated for loops and the read function to display all at once.

--- AtmMachine.java
+++ AtmMachine.java
@@ -3,100 +3,85 @@ import java.util.ArrayList;
 
 public class AtmMachine {
 	static ArrayList<Account> accountsList = new ArrayList<Account>();
- 	
  	private static int iterator;
 
 	//Function to create a new account
 	private static void create() {
-		accountsList.add(new Account());
 
- 		try {
- 			String aux01 = JOptionPane.showInputDialog("Digite o número da conta: " + "[" + (iterator + 1) + "]");
- 			int accountNumber = Integer.parseInt(aux01);
- 			accountsList.get(iterator).setAccountNumber(accountNumber);
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você digitou não digitou o número corretamente. Tente Novamente");
-
- 			String aux01 = JOptionPane.showInputDialog("Digite o número da conta: " + "[" + (iterator + 1) + "]");
- 			int accountNumber = Integer.parseInt(aux01);
- 			accountsList.get(iterator).setAccountNumber(accountNumber);
- 		}
+		String aux01 = JOptionPane.showInputDialog("Digite o número da conta: " + "[" + (iterator + 1) + "]");
+		int accountNumber = Integer.parseInt(aux01);
 
  		String aux02 = JOptionPane.showInputDialog("Digite o nome do titular: ");
- 		accountsList.get(iterator).setName(aux02);
 
- 		try {
- 			String aux03 = JOptionPane.showInputDialog("Digite o saldo inicial: ");
- 			Float accountBalance = Float.parseFloat(aux03);
- 			accountsList.get(iterator).setBalance(accountBalance);
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você não digitou o saldo corretamente. Tente Novamente");
+		String aux03 = JOptionPane.showInputDialog("Digite o saldo inicial: ");
+		Float accountBalance = Float.parseFloat(aux03);
 
- 			String aux03 = JOptionPane.showInputDialog("Digite o saldo inicial: ");
- 			Float accountBalance = Float.parseFloat(aux03);
- 			accountsList.get(iterator).setBalance(accountBalance);
- 		}
-
- 		JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso!");
-
+		accountsList.add(new Account());
+		
+		accountsList.get(iterator).setAccountNumber(accountNumber);
+		accountsList.get(iterator).setName(aux02);
+		accountsList.get(iterator).setBalance(accountBalance);
+		
+		JOptionPane.showMessageDialog(null, "Cliente cadastrado com sucesso!");
  		iterator++;
  		ATM();
  	}
 
+	static String result = "";
+
 	//Function to display all accounts 
  	private static void read() {
- 		for (int i = 0; i < accountsList.size(); i++) {
- 			JOptionPane.showMessageDialog(null, "Número da Conta: " + accountsList.get(i).getAccountNumber() + "\nTitular: "
- 					+ accountsList.get(i).getName() + "\nSaldo:  R$ " + accountsList.get(i).getBalance());
- 		}
+		result = "";
 
+ 		for (Account acc : accountsList) {
+			result += "Conta: " + acc.getAccountNumber() + " Titular: " + acc.getName() + " Saldo:  R$ " + acc.getBalance() + "\n";
+		}
+		
+		JOptionPane.showMessageDialog(null, result);
  		ATM();
-
  	}
 
 	//Function to update an account 
  	private static void update() {
- 		try {
- 			String aux01 = JOptionPane.showInputDialog("Qual conta você deseja alterar?");
- 			int accountNumber = Integer.parseInt(aux01);
+		String aux01 = JOptionPane.showInputDialog("Qual conta você deseja alterar?");
+		int accountNumber = Integer.parseInt(aux01);
 
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				if (accountsList.get(i).getAccountNumber() == accountNumber) {
- 					String aux02 = JOptionPane
- 							.showInputDialog("\nO que desejas alterar? \n1)Nome \n2)Número da Conta \n3)Saldo");
- 					int op = Integer.parseInt(aux02);
+		for (Account acc : accountsList) {
+			if (acc.getAccountNumber() == accountNumber) {
+				String aux02 = JOptionPane
+						.showInputDialog("\nO que deseja alterar? \n1) Nome \n2) Número da Conta \n3) Saldo");
+				int op = Integer.parseInt(aux02);
 
- 					switch (op) {
- 						case 1:
- 							accountsList.get(i).setName(JOptionPane.showInputDialog("Digite o novo nome: "));
- 							break;
- 						case 2:
- 							accountsList.get(i).setAccountNumber(
- 									Integer.parseInt(JOptionPane.showInputDialog("Digite o novo número: ")));
- 							break;
- 						case 3:
- 							accountsList.get(i)
- 									.setBalance(Float.parseFloat(JOptionPane.showInputDialog("Digite o novo saldo: ")));
- 							break;
- 					}
- 					JOptionPane.showMessageDialog(null, "Cliente alterado com Sucesso!");
- 				}
- 			}
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showInternalMessageDialog(null, "Digite o número da conta corretamente");
- 			update();
- 		}
+				switch (op) {
+					case 1:
+						acc.setName(JOptionPane.showInputDialog("Digite o novo nome: "));
+						JOptionPane.showMessageDialog(null, "Cliente alterado com Sucesso!");
+						break;
+					case 2:
+						acc.setAccountNumber(Integer.parseInt(JOptionPane.showInputDialog("Digite o novo número: ")));
+						JOptionPane.showMessageDialog(null, "Cliente alterado com Sucesso!");
+						break;
+					case 3:
+						acc.setBalance(Float.parseFloat(JOptionPane.showInputDialog("Digite o novo saldo: ")));
+						JOptionPane.showMessageDialog(null, "Cliente alterado com Sucesso!");
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Escolha uma opção entre 1 e 3...");
+						update();
+				}
+			}
+		}
 
  		ATM();
  	}
  	
 	//Function to delete an account
  	private static void delete() {
-		String n = JOptionPane.showInputDialog("Insira o número da conta que deseja excluir:");
-		int nmr = Integer.parseInt(n);
+		String auxiliar = JOptionPane.showInputDialog("Insira o número da conta que deseja excluir:");
+		int accountNumber = Integer.parseInt(auxiliar);
 		
 		for(Account account : accountsList){
-			if(account.getAccountNumber() == nmr){
+			if(account.getAccountNumber() == accountNumber){
 				accountsList.remove(account);
 
 				JOptionPane.showMessageDialog(null, "Conta removida com sucesso!");
@@ -108,37 +93,29 @@ public class AtmMachine {
 		
 		iterator--;
 		ATM();
-		
 	}
 
 	//Function to get the highest balance among all the accounts
  	private static float max() {
  		float max = accountsList.get(0).getBalance();
 
- 		for (int i = 0; i < accountsList.size(); i++) {
- 			try {
- 				if (accountsList.get(i).getBalance() > max) {
- 					max = accountsList.get(i).getBalance();
- 				}
- 			} catch (NullPointerException e) {
- 				continue;
- 			}
-
+ 		for (Account acc : accountsList) {
+			if (acc.getBalance() > max) {
+				max = acc.getBalance();
+			}
  		}
+
  		return max;
  	}
 
 	//function to get the lowest balance among all the accounts
  	private static float min() {
  		float min = accountsList.get(0).getBalance();
- 		for (int i = 0; i < accountsList.size(); i++) {
- 			try {
- 				if (accountsList.get(i).getBalance() < min) {
- 					min = accountsList.get(i).getBalance();
- 				}
- 			} catch (NullPointerException e) {
- 				continue;
- 			}
+
+ 		for (Account acc : accountsList) {
+			if (acc.getBalance() < min) {
+				min = acc.getBalance();
+			}
  		}
 
  		return min;
@@ -147,15 +124,14 @@ public class AtmMachine {
 	//Function to get the average balance of all accounts 
  	private static float average() {
  		float add = 0;
- 		for (int i = 0; i < accountsList.size(); i++) {
- 			try {
- 				add += accountsList.get(i).getBalance();
- 			} catch (NullPointerException e) {
- 				continue;
- 			}
+		int divider = 0;
+
+ 		for (Account acc : accountsList) {
+			divider++;
+			add += acc.getBalance();
  		}
 
- 		float average = add / iterator;
+ 		float average = add / divider;
  		return average;
  	}
 
@@ -174,41 +150,34 @@ public class AtmMachine {
 
 	//Function to take a bank statement
  	private static void extrato() {
- 		try {
- 			String aux = JOptionPane.showInputDialog("\nDe qual conta deseja tirar o extrato?");
- 			int accountNumber = Integer.parseInt(aux);
+		String aux = JOptionPane.showInputDialog("\nDe qual conta deseja tirar o extrato?");
+		int accountNumber = Integer.parseInt(aux);
 
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				if (accountsList.get(i).getAccountNumber() == accountNumber) {
- 					JOptionPane.showMessageDialog(null, "\nSaldo disponível: R$" + accountsList.get(i).getBalance());
- 				}
- 			}
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você não digitou o número da conta corretamente. Tente novamente.");
- 			extrato();
- 		}
+		for (Account acc : accountsList) {
+			if (acc.getAccountNumber() == accountNumber) {
+				JOptionPane.showMessageDialog(null, "\nSaldo disponível: R$ " + acc.getBalance());
+			}
+		}
+
  		ATM();
-
  	}
 
 	//Function to make a deposit
  	private static void depositar() {
- 		try {
- 			String aux01 = JOptionPane.showInputDialog("\nEm qual conta deseja realizar um depósito?");
- 			int accountNumber = Integer.parseInt(aux01);
- 			String aux02 = JOptionPane.showInputDialog("\nQual o valor do depósito?");
- 			Float depositValue = Float.parseFloat(aux02);
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				if (accountsList.get(i).getAccountNumber() == accountNumber) {
- 					float novoSaldo = accountsList.get(i).getBalance() + depositValue;
- 					accountsList.get(i).setBalance(novoSaldo);
- 					JOptionPane.showMessageDialog(null, "Depósito realizado com sucesso!");
- 				}
- 			}
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você não digitou o número da conta corretamente. Tente novamente.");
- 			depositar();
- 		}
+		String aux01 = JOptionPane.showInputDialog("\nEm qual conta deseja realizar um depósito?");
+		int accountNumber = Integer.parseInt(aux01);
+
+		for (Account acc : accountsList) {
+			if (acc.getAccountNumber() == accountNumber) {
+				String aux02 = JOptionPane.showInputDialog("\nQual o valor do depósito?");
+				Float depositValue = Float.parseFloat(aux02);		
+
+				float novoSaldo = acc.getBalance() + depositValue;
+				acc.setBalance(novoSaldo);
+
+				JOptionPane.showMessageDialog(null, "Depósito realizado com sucesso!");
+			}
+		}
 
  		ATM();
  	}
@@ -216,76 +185,65 @@ public class AtmMachine {
 	//Function to withdraw money from one account
  	private static void sacar() {
  		String aux01 = JOptionPane.showInputDialog("Em qual conta deseja realizar um saque?");
- 		try {
- 			int accountNumber = Integer.parseInt(aux01);
- 			String aux02 = JOptionPane.showInputDialog("Qual o valor do saque?");
- 			Float withdrawValue = Float.parseFloat(aux02);
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				if (accountsList.get(i).getAccountNumber() == accountNumber) {
- 					if (withdrawValue <= accountsList.get(i).getBalance()) {
- 						float newBalance = accountsList.get(i).getBalance() - withdrawValue;
- 						accountsList.get(i).setBalance(newBalance);
- 						JOptionPane.showMessageDialog(null, "Saque realizado com Sucesso!");
- 					} else {
- 						JOptionPane.showMessageDialog(null,
- 								"Valor do saque maior do que o saldo.\nSaque não concluído.");
- 					}
- 				}
- 			}
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você não digitou o número da conta corretamente. Tente novamente.");
- 			sacar();
- 		}
+		int accountNumber = Integer.parseInt(aux01);
+
+		for (Account acc : accountsList) {
+			if (acc.getAccountNumber() == accountNumber) {
+				String aux02 = JOptionPane.showInputDialog("Qual o valor do saque?");
+				Float withdrawValue = Float.parseFloat(aux02);
+
+				if (withdrawValue > acc.getBalance()) {
+					JOptionPane.showMessageDialog(null,	"Valor do saque maior do que o saldo.\nSaque não concluído.");
+				} else {
+					float newBalance = acc.getBalance() - withdrawValue;
+					acc.setBalance(newBalance);
+					JOptionPane.showMessageDialog(null, "Saque realizado com Sucesso!");
+				}
+			}
+		}
+
  		ATM();
  	}
 
 	//Function to transfer money from one account to another
  	private static void tranferir() {
- 		try {
- 			String aux01 = JOptionPane.showInputDialog("De qual conta deseja realizar uma transferência?");
- 			int originAccountNumber = Integer.parseInt(aux01);
- 			String aux02 = JOptionPane.showInputDialog("Qual o valor da transferência?");
- 			Float tedValue = Float.parseFloat(aux02);
- 			String aux03 = JOptionPane.showInputDialog("Para qual conta deseja enviar sua transferência?");
- 			int destinationAccountNumber = Integer.parseInt(aux03);
+		String aux01 = JOptionPane.showInputDialog("De qual conta deseja realizar uma transferência?");
+		int originAccountNumber = Integer.parseInt(aux01);
 
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				try {
- 					if (accountsList.get(i).getAccountNumber() == originAccountNumber) {
- 						if (tedValue <= accountsList.get(i).getBalance()) {
- 							float novoSaldoOrigem = accountsList.get(i).getBalance() - tedValue;
- 							accountsList.get(i).setBalance(novoSaldoOrigem);
- 							JOptionPane.showMessageDialog(null, "Transferência realizada com Sucesso!");
- 						} else {
- 							JOptionPane.showMessageDialog(null,
- 									"Valor da tranferência maior do que o saldo.\nTranferência não concluída.");
- 							ATM();
- 						}
- 					}
- 				} catch (NullPointerException e) {
- 					continue;
- 				}
- 			}
+		String aux02 = JOptionPane.showInputDialog("Qual o valor da transferência?");
+		Float tedValue = Float.parseFloat(aux02);		   
 
- 			for (int i = 0; i < accountsList.size(); i++) {
- 				try {
- 					if (accountsList.get(i).getAccountNumber() == destinationAccountNumber) {
- 						float novoSaldoDestino = accountsList.get(i).getBalance() + tedValue;
- 						accountsList.get(i).setBalance(novoSaldoDestino);
- 					}
- 				} catch (NullPointerException e) {
- 					continue;
- 				}
- 			}
+		String aux03 = JOptionPane.showInputDialog("Para qual conta deseja enviar sua transferência?");
+		int destinationAccountNumber = Integer.parseInt(aux03);
 
- 		} catch (NumberFormatException e) {
- 			JOptionPane.showMessageDialog(null, "Você digitou algo errado. Tente novamente.");
- 			tranferir();
- 		}
+		for (Account acc : accountsList) {
+				if (acc.getAccountNumber() == originAccountNumber) {
+
+					if (tedValue > acc.getBalance()) {
+							JOptionPane.showMessageDialog(null,
+							"Valor da tranferência maior do que o saldo.\nTranferência não concluída.");
+						ATM();
+					} else {
+						float novoSaldoOrigem = acc.getBalance() - tedValue;
+						acc.setBalance(novoSaldoOrigem);
+						JOptionPane.showMessageDialog(null, "Transferência realizada com Sucesso!");
+					}
+
+				}
+		}
+
+		for (Account acc : accountsList) {
+			if (acc.getAccountNumber() == destinationAccountNumber) {
+				float novoSaldoDestino = acc.getBalance() + tedValue;
+				acc.setBalance(novoSaldoDestino);
+			}
+		}
+
+ 		
  		ATM();
  	}
 
-	//Function to display the others functions and get the user's input and process it
+	//Function to display the others functions and get the user's input and manage it
 	 public static void ATM() {
 		try {
 			String aux = JOptionPane.showInputDialog(
@@ -338,7 +296,7 @@ public class AtmMachine {
 			}
 		} catch (NumberFormatException e) {
 			JOptionPane.showMessageDialog(null,
-					"Você não digitou a opção corretamente.\nDigite um número de 1 a 9 Por favor.");
+					"Você provavelmente digitou uma letra onde um número era esperado. Tente novamente...");
 			ATM();
 		}
 	}
